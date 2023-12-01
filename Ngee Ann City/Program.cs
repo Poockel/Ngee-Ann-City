c4c496f6e1264110a7496877c7c22b48dfad7bda
Wilson Lim <wilson.lim55555@gmail.com>
2023-12-02 02:33:32 +0800

    draw field

--- Ngee Ann City/Program.cs
+++ Ngee Ann City/Program.cs
@@ -1,5 +1,63 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Data;
+using System.Runtime.CompilerServices;
+using System.Linq.Expressions;
+using System;
+using System. Collections.Generic;
 
+IDictionary<string, int> game_vars = new Dictionary<string, int>();
+game_vars.Add("num_buildings", 0);
+game_vars.Add("points", 0);
+game_vars.Add("coins", 0);
+
+List<string> buildings = new List<string>();
+buildings.Add("R");
+buildings.Add("C");
+buildings.Add("I");
+buildings.Add("O");
+buildings.Add("*");
+
+void DrawField(int rows, int columns)
+{
+    char?[][] grid = new char?[rows][];
+    for (int i = 0; i < rows; i++)
+    {
+        grid[i] = new char?[columns];
+    }
+
+    Console.Write("    ");
+    for (int i = 1; i <= columns; i++)
+    {
+        Console.Write(String.Format("{0,4}",$"{i,3}"));
+    }
+    Console.WriteLine();
+
+    for (int i = 0; i < rows; i++)
+    {
+        Console.Write("    ");
+
+        for (int j = 0; j < columns; j++)
+        {
+            grid[i][j] = null;
+            Console.Write("+---");
+        }
+        Console.WriteLine("+");
+
+        Console.Write((char)('A' + i) + "   ");
+        Console.Write("");
+        for (int j = 0; j < columns; j++)
+        {
+            Console.Write("|   ");
+        }
+        Console.WriteLine("|");
+    }
+
+    Console.Write("    ");
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write("+---");
+    }
+    Console.WriteLine("+");
+}
 void Menu()
 {
     Console.WriteLine("------------------------");
@@ -10,9 +68,12 @@ void Menu()
     Console.WriteLine("------------------------");
 }
 
+
+
+
 void StartGame()
 {
-
+    DrawField(20,20);
 }
 
 void LoadSaved()
@@ -30,6 +91,10 @@ void ExitGame()
 
 }
 
+int i = 1;
+char c = (char)i;
+Console.WriteLine(c);
+
 while (true)
 {
     Menu();
@@ -41,10 +106,12 @@ while (true)
         if (Choice == 1)
         {
             StartGame();
+            break;
         }
         else if (Choice == 2)
         {
             LoadSaved();
+            break;
         }
         else if (Choice == 3)
         {
@@ -53,6 +120,8 @@ while (true)
         else if (Choice == 0)
         {
             ExitGame();
+            Console.WriteLine("Thanks for playing!");
+            break;
         }
         else
         {
@@ -61,6 +130,7 @@ while (true)
     }
     catch
     {
-        Console.WriteLine("Option must be an integer.");
+        Console.WriteLine("Select the number beside the option please.");
     }
 }
+
